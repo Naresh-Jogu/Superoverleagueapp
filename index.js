313ca5af41c0dc2af639018b3163dda75d44a0c8
Naresh Jogu <kumarn29346@gmail.com>
2026-01-13 04:26:29 +0000

    initial commit - Super Over League app

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div id='bg-container'>
+    <h1 className='heading'>Super Over Legue</h1>
+    <div>
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+        className='image-size'
+      />
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+        className='image-size'
+      />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
